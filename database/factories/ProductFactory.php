3a6dbb6a97cfd01e69754b99f48292edb4d207fd
yaoting <terry71210@gmail.com>
2020-07-07 21:29:57 +0800

    0707_09:43

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'title'       => $faker->company,
+        'description' => $faker->realText(100),
+        'image'       => $faker->imageUrl,
+        'on_sale'     => true,
+        'price'       => $faker->randomNumber(4),
     ];
 });
